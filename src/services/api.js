29f0f2670e6336b09d0d517ecdf0ad38c6c2b2f5
Johan Méndez <bitsadmin@MacBook-Pro-de-MacBook-2.local>
2018-03-01 15:43:05 -0500

    service uploaded

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,13 @@
 import apisauce from 'apisauce'
 
 // apisauce <3
-const create = (baseURL = 'https://api.myjson.com/bins') => {
+const create = (baseURL = 'https://jsonblob.com') => {
   const api = apisauce.create({
     baseURL
   })
 
   const languages = () =>
-    api.get('/aayrp')
+    api.get('/3d34167a-1d90-11e8-9810-3d6785638e2d')
 
   return {
     languages
